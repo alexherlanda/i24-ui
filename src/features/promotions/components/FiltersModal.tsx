30448de16ede82fe822ec9da4b3e5d064c6f514f
Alex <alexisherlanda.mx@gmail.com>
2024-04-16 12:16:25 -0600

    feat: clear promoters filters

--- src/features/promotions/components/FiltersModal.tsx
+++ src/features/promotions/components/FiltersModal.tsx
@@ -9,6 +9,7 @@ import {
   Button,
 } from '@chakra-ui/react';
 import { SelectionsSelect } from '../../electoralSections/components/SectionsSelect';
+import { useFormContext } from 'react-hook-form';
 
 type Props = {
   isOpen: boolean;
@@ -16,6 +17,13 @@ type Props = {
 };
 
 export const FiltersModal = ({ isOpen, onClose }: Props) => {
+  const { reset } = useFormContext();
+
+  const handleRest = () => {
+    reset();
+    onClose();
+  };
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -29,6 +37,9 @@ export const FiltersModal = ({ isOpen, onClose }: Props) => {
         </ModalBody>
 
         <ModalFooter>
+          <Button mr={3} onClick={handleRest}>
+            Borrar filtros
+          </Button>
           <Button colorScheme="blue" mr={3} onClick={onClose}>
             Aplicar filtros
           </Button>
